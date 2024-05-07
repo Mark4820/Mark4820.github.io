927a24353abeba4e70235117eb593763da714d98
Mark4820 <166926197+Mark4820@users.noreply.github.com>
2024-05-07 15:43:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let typed1 = new Typed ('.auto-input1',{
+    strings: [ 'Your Analyst', ' Your System Designer', 'And Your Future Bachelor Of Science In Information System', ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+  });
+  
+  let typed2 = new Typed ('.auto-input2',{
+    strings: [ ' "The best way to predict the future is to invent it" ' ],
+    typeSpeed: 300,
+    backSpeed: 150,
+    backDelay: 2000,
+    loop: true,
+  });
